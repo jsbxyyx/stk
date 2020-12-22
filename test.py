95acadd89c8a88b186dc3308dcaab5f6cd643f19
jsbxyyx <jsbxyyx@163.com>
2020-12-22 18:09:27 +0800

    fix: test case

--- test.py
+++ test.py
@@ -10,5 +10,9 @@ if __name__ == '__main__':
 
     Db.initConnectionPool()
 
-    klineInfo = Db.dbExecute(StockDao.selectLastKlineByCode, ('0.000001',))
-    print('date = {}'.format(klineInfo.date))
+    code = '0.000001'
+    klineInfo = Db.dbExecute(StockDao.selectLastKlineByCode, (code,))
+    if klineInfo is not None:
+        print('date = {}'.format(klineInfo.date))
+    else:
+        print('klineInfo not found, code={}'.format(code))
