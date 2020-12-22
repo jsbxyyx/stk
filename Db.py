ed95e0f484cc6591bd885fddd2a029d35753be6a
jsbxyyx <jsbxyyx@163.com>
2020-12-22 16:57:41 +0800

    use db pool

--- Db.py
+++ Db.py
@@ -1,19 +1,44 @@
 import mariadb
-import sys
 
-conn = None
+pool = None
+user = "root"
+password = "mariadb"
+host = "192.168.1.9"
+port = 3306
+database = "istock"
 
 
-def initConnection():
+def initConnectionPool():
+    global pool
+    pool = mariadb.ConnectionPool(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database,
+        pool_name="app",
+        pool_size=8
+    )
+    return pool
+
+
+def getConnection():
     try:
-        global conn
-        conn = mariadb.connect(user="root", password="mariadb", host="192.168.1.9", port=3306, database="istock")
+        conn = pool.get_connection()
     except mariadb.Error as e:
         print(f'Error connecting to MariaDB Platform: {e}')
-        sys.exit(1)
+    return conn
 
 
 def dbExecute(fun, params=()):
-    cursor = conn.cursor()
-    fun(cursor, params)
-    conn.commit()
+    try:
+        conn = getConnection()
+        cursor = conn.cursor()
+        result = fun(cursor, params)
+        conn.commit()
+        return result
+    except Exception as e:
+        print(f'dbExecute error. {e}')
+    finally:
+        conn.close()
+
